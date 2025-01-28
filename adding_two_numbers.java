9afc95a670fd8e3d2b19c25fb72f2978d3e222a3
MONISH N <monishnarasimman2005@gmail.com>
2025-01-28 18:08:04 +0530

    Update adding_two_numbers.java

--- adding_two_numbers.java
+++ adding_two_numbers.java
@@ -3,12 +3,21 @@
 
 class Practice{
     public static void main(String args[]){
-        int num1 = 20;
+        int num1 = 20; 
         int num2 = 30;
-        System.out.println("First Number : " + num1);
-        System.out.println("Second Number : " + num2);
+        System.out.println("First Number : " + num1); 
+        System.out.println("Second Number : " + num2); 
         System.out.print("Total is ");
 
         System.out.println(num1+num2);
     }
 }
+
+
+//the program calculates the sum of the two digits which is assigned to the  "num1" & "num2" using the "+" operator
+
+//RESULT:
+
+// First Number : 20
+// Second Number : 30
+// Total is 50
