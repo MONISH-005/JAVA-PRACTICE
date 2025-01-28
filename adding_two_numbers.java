a336fc001318ac5eed5671b34b936260aa4e7195
MONISH N <monishnarasimman2005@gmail.com>
2025-01-28 18:00:30 +0530

    Create adding_two_numbers.java

---
+++ adding_two_numbers.java
@@ -0,0 +1,14 @@
+//Day 2
+// ex-1
+
+class Practice{
+    public static void main(String args[]){
+        int num1 = 20;
+        int num2 = 30;
+        System.out.println("First Number : " + num1);
+        System.out.println("Second Number : " + num2);
+        System.out.print("Total is ");
+
+        System.out.println(num1+num2);
+    }
+}
